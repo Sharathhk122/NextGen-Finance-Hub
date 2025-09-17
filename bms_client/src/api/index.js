a45ea8d78482540a7045651bc76fa373e52033da
Sharath H K <158291023+Sharathhk122@users.noreply.github.com>
2025-09-17 10:23:01 +0530

    Update index.js

--- bms_client/src/api/index.js
+++ bms_client/src/api/index.js
@@ -1,7 +1,7 @@
 // src/api/index.js
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://backend-hk.onrender.com/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://bmsp-back.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
